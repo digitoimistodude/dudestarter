3eca68dd5fc2779bb261449778b6a0ce7a59118b
Roni Laukkarinen <roni@laukkarinen.info>
2015-09-07 14:35:42 +0300

    Optimization: Remove query strings from static resources

--- functions.php
+++ functions.php
@@ -6,6 +6,20 @@
  */
 
 /*
+ * Remove query strings from static resources
+ *
+ * @link https://wordpress.org/support/topic/how-to-remove-query-strings-from-static-resources
+ */
+
+function _remove_script_version( $src ){
+  $parts = explode( '?', $src );
+  return $parts[0];
+}
+add_filter( 'script_loader_src', '_remove_script_version', 15, 1 );
+add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
+
+
+/*
  * ACF options page
  */
 if( function_exists('acf_add_options_page') ) {
